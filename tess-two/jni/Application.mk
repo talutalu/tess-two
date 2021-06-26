75e63849b9e3c1378a088f801f198c438dd07ab8
chris <cfornaro@gmail.com>
2021-06-27 00:39:17 +0200

    remove x86 and x86_64 targets

--- tess-two/jni/Application.mk
+++ tess-two/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := c++_static
-APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
+APP_ABI := armeabi-v7a arm64-v8a
 APP_OPTIM := release
 APP_PLATFORM := android-16
 APP_CPPFLAGS += -fexceptions -frtti
